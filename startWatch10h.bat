65e135b45324c7fcb934151e409b33b569a72de7
HGriesbeck <75359774+HGriesbeck@users.noreply.github.com>
2020-12-23 15:04:41 +0100

    erste Version von Watchdog

---
+++ startWatch10h.bat
@@ -0,0 +1,15 @@
+rem @echo on
+
+REM **********************************************
+REM Batch file for starting WatchDog10h  on Windows
+REM **********************************************
+
+SET LOCALCLASSPATH=.\watchdog10h.jar;
+
+SET LOCAL_JAVA_HOME=.\jre
+
+%LOCAL_JAVA_HOME%\bin\javaw -classpath %LOCALCLASSPATH% harri.time.Watch10h
+
+set LOCALCLASSPATH=
+
+rem pause
